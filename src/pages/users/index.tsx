6c8dd14680be62df2fb8285114df798378076092
gabriel.barreto <hp18360515>
2021-04-30 00:51:38 -0300

    feat: listagem responsiva.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
   Tr,
   HStack,
   theme,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import {
   RiAddLine,
@@ -22,11 +23,19 @@ import {
   RiEditLine,
   RiPencilLine,
 } from "react-icons/ri";
+
+import HorizontalScroll from "react-scroll-horizontal";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -49,23 +58,25 @@ export default function UserList() {
               Novo usuário
             </Button>
           </Flex>
+
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                {/* <Th px={["4", "4","6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
-                </Th>
+                </Th> */}
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th w="8">Ações</Th>
               </Tr>
             </Thead>
+
             <Tbody>
               <Tr>
-                <Td px="6">
+                {/* <Td px={["4", "4","6"]}>
                   <Checkbox colorScheme="pink" />
-                </Td>
-                <Td px="6">
+                </Td> */}
+                <Td px={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight="bold">Gabriel Santos</Text>
                     <Text fontSize="sm" color="gray.300">
@@ -73,8 +84,80 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td px="6">15 de Fevereiro, 2021</Td>
-                <Td px="6">
+                {isWideVersion && <Td px="6">15 de Fevereiro, 2021</Td>}
+                <Td px={["4", "4", "6"]}>
+                  <HStack>
+                    <Button
+                      as="a"
+                      cursor="pointer"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiEditLine} />
+                    </Button>
+                    <Button
+                      as="a"
+                      cursor="pointer"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiDeleteBinLine} />
+                    </Button>
+                  </HStack>
+                </Td>
+              </Tr>
+              <Tr>
+                {/* <Td px={["4", "4","6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td> */}
+                <Td px={["4", "4", "6"]}>
+                  <Box>
+                    <Text fontWeight="bold">Gabriel Santos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gabriel.limabarreto@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td px="6">15 de Fevereiro, 2021</Td>}
+                <Td px={["4", "4", "6"]}>
+                  <HStack>
+                    <Button
+                      as="a"
+                      cursor="pointer"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiEditLine} />
+                    </Button>
+                    <Button
+                      as="a"
+                      cursor="pointer"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiDeleteBinLine} />
+                    </Button>
+                  </HStack>
+                </Td>
+              </Tr>
+              <Tr>
+                {/* <Td px={["4", "4","6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td> */}
+                <Td px={["4", "4", "6"]}>
+                  <Box>
+                    <Text fontWeight="bold">Gabriel Santos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gabriel.limabarreto@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td px="6">15 de Fevereiro, 2021</Td>}
+                <Td px={["4", "4", "6"]}>
                   <HStack>
                     <Button
                       as="a"
