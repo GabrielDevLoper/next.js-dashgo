1c46ec75e5bb34de1636edf627273a4833617cd4
gabriel.barreto <hp18360515>
2021-04-29 22:58:31 -0300

    feat: responsividade no dashboard.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
           minChildWidth="320px"
           align="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
@@ -83,7 +83,7 @@ export default function Dashboard() {
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
